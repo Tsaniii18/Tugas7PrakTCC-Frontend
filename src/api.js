4cd097979810d599fe1010cbb4080d2ca0ff8ba1
tsani31 <tsaniii.18@gmail.com>
2025-05-31 01:46:32 +0700

    restore url backend

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { jwtDecode } from "jwt-decode";
 
 const api = axios.create({
-  baseURL: 'https://backend-722144796089.us-central1.run.app/ws',
+  baseURL: 'https://backend-722144796089.us-central1.run.app',
   withCredentials: true
 });
 
